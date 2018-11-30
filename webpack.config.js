feb4e555ce8ed44d8a7951587f22cd9bcc4fd9b1
徐同保 <xutongbao@vackbot.com>
2018-11-30 17:24:51 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+	mode: 'development',
+	entry: './hello.js',
+	devtool: 'cheap-source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true
+          }
+        }
+      }   
+    ]
+  }
+}
